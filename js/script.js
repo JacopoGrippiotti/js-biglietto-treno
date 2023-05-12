e8bb377e44b9fae4c1fefcf16fd1ab8fbfb2e075
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-05-12 15:29:02 +0200

    added variable scontoVenti and variable scontoQuaranta to script.js file

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 let kmDaPercorrere = prompt("Quanti chilometri devi percorrere con il treno?");
 
-let etàPasseggero = parseInt(prompt("inserisci la tua età"))
+let etàPasseggero = parseInt(prompt("inserisci la tua età"));
+
+let prezzoBiglietto = 0.21 * kmDaPercorrere;
+
+let scontoVenti = 20 / 100;
+
+let scontoQuaranta = 40 / 100;
+
+if (etàPasseggero < 18);{
+    
+    prezzoBiglietto -= (scontoVenti * prezzoBiglietto);
+}
+
+document.getElementById("gino").innerHTML = prezzoBiglietto;
